c6292fa875929928d0bff91f981dcb4041908ed8
Guy Leonard <guy.leonard@gmail.com>
2017-03-24 15:40:55 +0000

    section 3 populated, I think

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@
 
 echo Step 1. Set up mySQL container
 docker run -d \
-           --name hyphochytrium_mysql \
+           --name hyphochytrium-mysql \
            -e MYSQL_ROOT_PASSWORD=rootuserpassword \
            -e MYSQL_ROOT_HOST='172.17.0.0/255.255.0.0' \
            mysql/mysql-server:5.5 &&
@@ -26,8 +26,19 @@ sleep 10 &&
 
 echo Step 2. Set up template database using EasyMirror &&
 docker run --rm \
-           --name genomehubs-ensembl \
-           -v ~/demo/genomehubs-import/ensembl/conf:/ensembl/conf:ro \
-           --link genomehubs-mysql \
+           --name hyphochytrium-ensembl \
+           -v ~/hyphochytrium_genomehub/import/ensembl/conf:/ensembl/conf:ro \
+           --link hyphochytrium-mysql \
            -p 8081:8080 \
           genomehubs/easy-mirror:17.03.23 /ensembl/scripts/database.sh /ensembl/conf/database.ini &&
+
+echo Step 3. Import sequences, prepare gff and import gene models &&
+docker run --rm \
+           -u $UID:$GROUPS \
+           --name easy-import-hyphochytrium-catenoides \
+           --link hyphochytrium-mysql \
+           -v ~/hyphochytrium_genomehub/import/hypho/conf:/import/conf \
+           -v ~/hyphochytrium_genomehub/import/hypho/data:/import/data \
+           -e DATABASE=Hyphochytrium_catenoides_2017 \
+           -e FLAGS="-s -p -g" \
+           genomehubs/easy-import:17.03.23 &&
